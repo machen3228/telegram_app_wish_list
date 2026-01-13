435dadf6e1c09a6fd7c52e2a10d511b3bdf3835d
machen <machen3228@gmail.com>
2026-01-13 23:42:03 +0300

    Update openapi schema

--- app/controllers/users.py
+++ app/controllers/users.py
@@ -9,28 +9,29 @@ from services.users import UserService
 
 class UserController(Controller):
     path = '/users'
+    tags = ('Users',)
 
-    @post(status_code=201)
+    @post(status_code=201, summary='Add new user')
     async def add(self, data: UserCreateDTO) -> dict[str, int]:
         async with get_session() as session:
             service = UserService(session)
             tg_id = await service.add(**data.__dict__)
             return {'tg_id': tg_id}
 
-    @get('/{tg_id:int}', return_dto=DataclassDTO[User])
+    @get('/{tg_id:int}', return_dto=DataclassDTO[User], summary='Get user')
     async def get(self, tg_id: int) -> User:
         async with get_session() as session:
             service = UserService(session)
             return await service.get(tg_id)
 
-    @post('/{tg_id:int}/{friend_id:int}', status_code=201)
+    @post('/{tg_id:int}/{friend_id:int}', status_code=201, summary='Add new friend')
     async def add_friend(self, tg_id: int, friend_id: int) -> dict[str, str]:
         async with get_session() as session:
             service = UserService(session)
             await service.add_friend(tg_id, friend_id)
             return {'message': 'Friend was added'}
 
-    @delete('/{tg_id:int}/{friend_id:int}', status_code=204)
+    @delete('/{tg_id:int}/{friend_id:int}', status_code=204, summary='Delete friend')
     async def delete_friend(self, tg_id: int, friend_id: int) -> None:
         async with get_session() as session:
             service = UserService(session)
