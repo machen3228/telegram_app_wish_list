c89ea449ecf03e241305e62c5f54cbcea1893e8e
machen <machen3228@gmail.com>
2026-02-17 22:36:43 +0300

    fix: add annotation to config

--- app/core/config/app.py
+++ app/core/config/app.py
@@ -2,10 +2,10 @@ from pydantic import BaseModel
 
 
 class AppConfig(BaseModel):
-    title = 'Wishlist API'
-    version = '0.0.1'
-    description = 'Backend API for Telegram wishlist app'
-    developer_name = 'developer'
-    developer_email = 'machen3228@gmail.com'
+    title: str = 'Wishlist API'
+    version: str = '0.0.1'
+    description: str = 'Backend API for Telegram wishlist app'
+    developer_name: str = 'developer'
+    developer_email: str = 'machen3228@gmail.com'
 
     max_tg_token_age: int = 86400
