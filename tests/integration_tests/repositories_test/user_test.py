5657034cf1850567e3fd46721a36a15270e36a76
machen <machen3228@gmail.com>
2026-02-20 18:00:38 +0300

    test: add tests for update user repo method

--- tests/integration_tests/repositories_test/user_test.py
+++ tests/integration_tests/repositories_test/user_test.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from hamcrest import all_of
 from hamcrest import assert_that
 from hamcrest import equal_to
+from hamcrest import greater_than
 from hamcrest import has_properties
 from hamcrest import instance_of
 import pytest
@@ -42,3 +43,87 @@ class TestUserRepository:
     ) -> None:
         with pytest.raises(NotFoundInDbError, match=r'User with id=\d+ not found'):
             await user_repository.get(123456)
+
+    async def test_repo_update_user_success(
+        self,
+        user_repository: UserRepository,
+        test_user: UserDict,
+    ) -> None:
+        await user_repository.update(test_user['tg_id'], tg_username='new_username', first_name='new_first_name')
+        updated_user = await user_repository.get(test_user['tg_id'])
+
+        assert_that(
+            updated_user,
+            has_properties(
+                tg_username=equal_to('new_username'),
+                first_name=equal_to('new_first_name'),
+            ),
+        )
+
+    async def test_repo_update_user_updates_updated_at(
+        self,
+        user_repository: UserRepository,
+        test_user: UserDict,
+    ) -> None:
+        await user_repository.update(test_user['tg_id'], first_name='new_first_name')
+        updated_user = await user_repository.get(test_user['tg_id'])
+
+        assert_that(
+            updated_user.updated_at,
+            greater_than(test_user['updated_at']),
+        )
+
+    async def test_repo_update_user_does_not_change_other_fields(
+        self,
+        user_repository: UserRepository,
+        test_user: UserDict,
+    ) -> None:
+        await user_repository.update(test_user['tg_id'], first_name='new_first_name')
+        updated_user = await user_repository.get(test_user['tg_id'])
+
+        assert_that(
+            updated_user,
+            has_properties(
+                tg_username=equal_to(test_user['tg_username']),
+                last_name=equal_to(test_user['last_name']),
+                avatar_url=equal_to(test_user['avatar_url']),
+            ),
+        )
+
+    async def test_repo_update_user_no_fields_does_nothing(
+        self,
+        user_repository: UserRepository,
+        test_user: UserDict,
+    ) -> None:
+        await user_repository.update(test_user['tg_id'])
+        user = await user_repository.get(test_user['tg_id'])
+
+        assert_that(
+            user,
+            has_properties(
+                tg_username=equal_to(test_user['tg_username']),
+                first_name=equal_to(test_user['first_name']),
+                updated_at=equal_to(test_user['updated_at']),
+            ),
+        )
+
+    @pytest.mark.parametrize(
+        ('field', 'value'),
+        [
+            pytest.param('tg_username', 'updated_username', id='update_tg_username'),
+            pytest.param('first_name', 'updated_first_name', id='update_first_name'),
+            pytest.param('last_name', 'updated_last_name', id='update_last_name'),
+            pytest.param('avatar_url', 'updated_avatar_url', id='update_avatar_url'),
+        ],
+    )
+    async def test_repo_update_user_single_field(
+        self,
+        user_repository: UserRepository,
+        test_user: UserDict,
+        field: str,
+        value: str,
+    ) -> None:
+        await user_repository.update(test_user['tg_id'], **{field: value})
+        updated_user = await user_repository.get(test_user['tg_id'])
+
+        assert_that(updated_user, has_properties(**{field: equal_to(value)}))
