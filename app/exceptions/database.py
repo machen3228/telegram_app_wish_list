12f4063e8a26ce1484117272b8a6d4985e528d61
machen <machen3228@gmail.com>
2026-02-20 23:17:14 +0300

    feat(exception): add AlreadyExistsInDbError

--- app/exceptions/database.py
+++ app/exceptions/database.py
@@ -3,3 +3,10 @@ class NotFoundInDbError(LookupError):
         self.entity = entity
         self.entity_id = entity_id
         super().__init__(f'{entity} with id={entity_id} not found')
+
+
+class AlreadyExistsInDbError(LookupError):
+    def __init__(self, entity: str, field: int | str) -> None:
+        self.entity = entity
+        self.field = field
+        super().__init__(f'{entity} with this {field!r} already exists')
