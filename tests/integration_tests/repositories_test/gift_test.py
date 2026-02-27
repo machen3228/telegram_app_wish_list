1ef894f1efee6b622127387402f5ac2b7547b9c5
machen <machen3228@gmail.com>
2026-02-27 20:05:43 +0300

    test: add tests for get gift

--- tests/integration_tests/repositories_test/gift_test.py
+++ tests/integration_tests/repositories_test/gift_test.py
@@ -1,6 +1,8 @@
 from hamcrest import assert_that
 from hamcrest import equal_to
 from hamcrest import has_properties
+from hamcrest import is_
+from hamcrest import none
 from hamcrest import not_none
 import pytest
 from sqlalchemy import text
@@ -83,3 +85,94 @@ class TestGiftRepository:
         gift_repository: GiftRepository,
     ) -> None:
         await gift_repository.delete(123456)
+
+    async def test_repo_get_gift_success(
+        self,
+        gift_repository: GiftRepository,
+        test_bob_gift: GiftDict,
+    ) -> None:
+        result = await gift_repository.get(
+            test_bob_gift['id'],
+            test_bob_gift['user_id'],
+        )
+
+        assert_that(
+            result,
+            has_properties(
+                id=equal_to(test_bob_gift['id']),
+                user_id=equal_to(test_bob_gift['user_id']),
+                name=equal_to(test_bob_gift['name']),
+                url=equal_to(test_bob_gift['url']),
+                wish_rate=equal_to(test_bob_gift['wish_rate']),
+                price=equal_to(test_bob_gift['price']),
+                note=equal_to(test_bob_gift['note']),
+                is_reserved=is_(False),
+                reserved_by=is_(none()),
+            ),
+        )
+
+    async def test_repo_get_gift_not_found_raises(
+        self,
+        gift_repository: GiftRepository,
+        test_user_bob: UserDict,
+    ) -> None:
+        with pytest.raises(NotFoundInDbError, match='Gift with id=123456 not found'):
+            await gift_repository.get(123456, test_user_bob['tg_id'])
+
+    async def test_repo_get_gift_reserved_by_current_user(
+        self,
+        gift_repository: GiftRepository,
+        test_bob_gift: GiftDict,
+        test_user_john: UserDict,
+        db_session: AsyncSession,
+    ) -> None:
+        await db_session.execute(
+            text("""
+                 INSERT INTO gift_reservations (gift_id, reserved_by_tg_id)
+                 VALUES (:gift_id, :reserved_by_tg_id)
+                 """),
+            {'gift_id': test_bob_gift['id'], 'reserved_by_tg_id': test_user_john['tg_id']},
+        )
+        await db_session.flush()
+
+        result = await gift_repository.get(
+            test_bob_gift['id'],
+            test_user_john['tg_id'],
+        )
+
+        assert_that(
+            result,
+            has_properties(
+                is_reserved=is_(True),
+                reserved_by=equal_to(test_user_john['tg_id']),
+            ),
+        )
+
+    async def test_repo_get_gift_reserved_by_other_user(
+        self,
+        gift_repository: GiftRepository,
+        test_bob_gift: GiftDict,
+        test_user_john: UserDict,
+        db_session: AsyncSession,
+    ) -> None:
+        await db_session.execute(
+            text("""
+                 INSERT INTO gift_reservations (gift_id, reserved_by_tg_id)
+                 VALUES (:gift_id, :reserved_by_tg_id)
+                 """),
+            {'gift_id': test_bob_gift['id'], 'reserved_by_tg_id': test_user_john['tg_id']},
+        )
+        await db_session.flush()
+
+        result = await gift_repository.get(
+            test_bob_gift['id'],
+            test_bob_gift['user_id'],
+        )
+
+        assert_that(
+            result,
+            has_properties(
+                is_reserved=is_(True),
+                reserved_by=is_(none()),
+            ),
+        )
