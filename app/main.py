ee788098b36b3d73c4d16838e99f602118583af4
machen <machen3228@gmail.com>
2026-03-10 21:35:40 +0300

    ci: made preparations for production

--- app/main.py
+++ app/main.py
@@ -3,4 +3,4 @@ import uvicorn
 import application  # noqa: F401
 
 if __name__ == '__main__':
-    uvicorn.run('application:app', host='0.0.0.0', port=80, reload=True)  # noqa: S104
+    uvicorn.run('application:app', host='0.0.0.0', port=80, reload=False)  # noqa: S104
